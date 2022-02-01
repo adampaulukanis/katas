a1d369b425ae67916b3d643876a76c22b73a5439
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-02-01 20:59:01 +0000

    Change one condition
    Watching one youtuber (Tsodin) I spotted he was solving the Fizz Buzz and
    he used one condition as I tried to replicate it here.

--- fizzbuzz/node/index.js
+++ fizzbuzz/node/index.js
@@ -1,15 +1,11 @@
-'use strict'
+'use strict';
 
-function fizzBuzz(number){
-    if (number % 3 === 0 && number % 5 === 0)
-        return 'Fizz Buzz'
-    else if (number % 3 === 0)
-        return 'Fizz'
-    else if (number % 5 === 0)
-        return 'Buzz'
-    else
-        return number
+function fizzBuzz(number) {
+  // if (number % 3 === 0 && number % 5 === 0)
+  if (number % (3 * 5) === 0) return 'Fizz Buzz';
+  else if (number % 3 === 0) return 'Fizz';
+  else if (number % 5 === 0) return 'Buzz';
+  else return number;
 }
 
-module.exports = fizzBuzz
-
+module.exports = fizzBuzz;
