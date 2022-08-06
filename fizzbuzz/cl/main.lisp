665008eabebd4fd671d449d01560174acb8a11c5
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-08-07 00:58:49 +0100

    Add something

--- fizzbuzz/cl/main.lisp
+++ fizzbuzz/cl/main.lisp
@@ -1,5 +1,17 @@
+(defun fizz (number)
+  (let ((f (mod number 3)))
+    (if (= f 0)
+        'yo
+        'no)))
+
+(fizz 5)
+
 (loop for i from 0 to 100
       do
+      (princ i)
+      (fizz i))
+
+(defun x ()
       (let ((fizz (= (mod i 3) 0))
             (buzz (= (mod i 5) 0)))
         (princ i)
