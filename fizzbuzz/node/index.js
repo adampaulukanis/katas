a3d88c48480f5534b3b39d32203f30ed12941480
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-08-10 13:39:26 +0100

    fizzbuzz: get rid of an old comment in the code

--- fizzbuzz/node/index.js
+++ fizzbuzz/node/index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 function fizzBuzz(number) {
-  // if (number % 3 === 0 && number % 5 === 0)
-  if (number % (3 * 5) === 0) return 'Fizz Buzz';
-  else if (number % 3 === 0) return 'Fizz';
-  else if (number % 5 === 0) return 'Buzz';
-  else return number;
+    if (number % (3 * 5) === 0) return 'Fizz Buzz';
+    else if (number % 3 === 0) return 'Fizz';
+    else if (number % 5 === 0) return 'Buzz';
+    else return number;
 }
 
 module.exports = fizzBuzz;
